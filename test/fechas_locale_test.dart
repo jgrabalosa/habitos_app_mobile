4707968a48c103c7d8eddfcec85d6f75d803ca22
Joaquim Grabalosa <joaquim.grabalosa.gest@gmail.com>
2026-09-10 23:43:00 +0200

    Cabecera de Hoy: formatearTituloDelDia extraido y con tests (Codex, tests sin ejecutar)

--- test/fechas_locale_test.dart
+++ test/fechas_locale_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:habitos_app_mobile/screens/dashboard_screen.dart';
 
 /// Comprueba que los formatos de fecha realmente cambian con el idioma.
 /// Si hiciera falta initializeDateFormatting, sin él estos asserts fallarían.
@@ -31,4 +32,49 @@ void main() {
     expect(DateFormat.MMMMd('en').format(fecha).toLowerCase(), contains('june'));
     expect(DateFormat.MMMMd('pt').format(fecha).toLowerCase(), contains('junho'));
   });
+
+  test('la cabecera de hoy usa MMMMd sin día de la semana', () {
+    final fecha = DateTime(2026, 9, 9);
+
+    for (final locale in ['es', 'en', 'pt']) {
+      final fechaSinSemana = DateFormat.MMMMd(locale).format(fecha);
+      final fechaConSemana = DateFormat.MMMMEEEEd(locale).format(fecha);
+      final titulo = formatearTituloDelDia(
+        fecha: fecha,
+        locale: locale,
+        textoHoy: _textoHoy(locale),
+        viendoHoy: true,
+      );
+
+      expect(titulo, '${_textoHoy(locale)}, $fechaSinSemana');
+      expect(titulo, isNot(contains(fechaConSemana)));
+    }
+  });
+
+  test('la cabecera de otro día usa MMMMEEEEd', () {
+    final fecha = DateTime(2026, 9, 9);
+
+    for (final locale in ['es', 'en', 'pt']) {
+      final largo = DateFormat.MMMMEEEEd(locale).format(fecha);
+      final titulo = formatearTituloDelDia(
+        fecha: fecha,
+        locale: locale,
+        textoHoy: _textoHoy(locale),
+        viendoHoy: false,
+      );
+
+      expect(titulo, '${largo[0].toUpperCase()}${largo.substring(1)}');
+      expect(
+        titulo.toLowerCase(),
+        contains(DateFormat.MMMM(locale).format(fecha).toLowerCase()),
+      );
+    }
+  });
 }
+
+String _textoHoy(String locale) => switch (locale) {
+      'es' => 'Hoy',
+      'en' => 'Today',
+      'pt' => 'Hoje',
+      _ => throw ArgumentError(locale),
+    };
